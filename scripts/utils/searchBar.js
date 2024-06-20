989475a49941d82fba032c42d54653fad7752e08
Maxime Brunet <maximb84@gmail.com>
2024-06-20 22:00:49 +0200

    style : algorithme boucles natives

--- scripts/utils/searchBar.js
+++ scripts/utils/searchBar.js
@@ -5,9 +5,28 @@ export function searchRecipes(search, recipes) {
 
     for (let i = 0; i < recipes.length; i++) {
         let recipe = recipes[i];
-        if (recipe.name.toLowerCase().includes(search) ||
-            recipe.ingredients.some(ingredient => ingredient.ingredient.toLowerCase().includes(search)) ||
-            recipe.description.toLowerCase().includes(search)) {
+        let found = false;
+
+        if (recipe.name.toLowerCase().indexOf(search) !== -1) {
+            found = true;
+        }
+
+        if (!found) {
+            for (let j = 0; j < recipe.ingredients.length; j++) {
+                if (recipe.ingredients[j].ingredient.toLowerCase().indexOf(search) !== -1) {
+                    found = true;
+                    break;
+                }
+            }
+        }
+
+        if (!found) {
+            if (recipe.description.toLowerCase().indexOf(search) !== -1) {
+                found = true;
+            }
+        }
+
+        if (found) {
             results.push(recipe);
         }
     }
