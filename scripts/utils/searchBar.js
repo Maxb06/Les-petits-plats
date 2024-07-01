ffcc0678159ad61331ab25b6340c16f7fb97dbf6
Maxime Brunet <maximb84@gmail.com>
2024-07-01 15:49:12 +0200

    style : instruction 'continue' / ajout jsdoc

--- scripts/utils/searchBar.js
+++ scripts/utils/searchBar.js
@@ -1,35 +1,38 @@
-// Fonction de recherche boucles natives
+/**
+ * Function to search for recipes using native loops.
+ *
+ * @param {string} search - The search term to filter recipes by.
+ * @param {Array} recipes - The array of recipe objects to search within.
+ * @returns {Array} - The array of recipes that match the search term.
+ */
 export function searchRecipes(search, recipes) {
     let results = [];
     search = search.toLowerCase();
 
     for (let i = 0; i < recipes.length; i++) {
         let recipe = recipes[i];
-        let found = false;
 
         if (recipe.name.toLowerCase().indexOf(search) !== -1) {
-            found = true;
+            results.push(recipe);
+            continue;
         }
 
-        if (!found) {
-            for (let j = 0; j < recipe.ingredients.length; j++) {
-                if (recipe.ingredients[j].ingredient.toLowerCase().indexOf(search) !== -1) {
-                    found = true;
-                    break;
-                }
+        for (let j = 0; j < recipe.ingredients.length; j++) {
+            if (recipe.ingredients[j].ingredient.toLowerCase().indexOf(search) !== -1) {
+                results.push(recipe);
+                break; // quitte la boucle ingredients
             }
         }
 
-        if (!found) {
-            if (recipe.description.toLowerCase().indexOf(search) !== -1) {
-                found = true;
-            }
+        if (results[results.length - 1] === recipe) {
+            continue; // passe àla prochaine recette si l'ingredient a été trouvé
         }
 
-        if (found) {
+        if (recipe.description.toLowerCase().indexOf(search) !== -1) {
             results.push(recipe);
         }
     }
     
     return results;
 }
+
